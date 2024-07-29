df3124139a9ee7b396e4f405954eb8e173eaa48e
남균 <gyuni9604@naver.com>
2024-07-29 23:46:28 +0900

    recoil 상태값 변경 , action 추가

---
+++ memo-front/src/reducers/actions.ts
@@ -0,0 +1,13 @@
+import { SetterOrUpdater } from 'recoil'
+
+// 로그인 액션
+export const login = (setUser: SetterOrUpdater<string>, username: string) => {
+  setUser(username)
+  localStorage.setItem('user', username)
+}
+
+// 로그아웃 액션
+export const logout = (setUser: SetterOrUpdater<string>) => {
+  setUser('')
+  localStorage.removeItem('user')
+}
--- memo-front/src/reducers/state.ts
+++ memo-front/src/reducers/state.ts
@@ -1,6 +1,16 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
 
-export const loginState = atom<string>({
-  key: 'loginState',
-  default: localStorage.getItem('login') || '',
+// 사용자 정보를 저장하는 atom
+export const userState = atom<string>({
+  key: 'userState',
+  default: localStorage.getItem('user') || '',
+})
+
+// 로그인 상태를 반환하는 selector
+export const isLoggedInState = selector({
+  key: 'isLoggedInState',
+  get: ({ get }) => {
+    const user = get(userState)
+    return user !== ''
+  },
 })
