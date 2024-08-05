788609098e75e28a2bbec5f694729b8508d66142
남균 <gyuni9604@naver.com>
2024-08-05 23:03:44 +0900

    ( 추가 ) 사이드 메뉴 추가

---
+++ memo-front/src/components/common/SideMenu.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import apiCall from '../../api/api'
+import { changeDisplayFolder } from '../../reducers/actions'
+import { useRecoilState } from 'recoil'
+import { displayFolderState } from '../../reducers/state'
+
+const SideMenu = () => {
+  const [folderList, setFolderList] = useState<any>([])
+  const [_, setDisplayFolder] = useRecoilState(displayFolderState)
+
+  useEffect(() => {
+    const fetchFolder = async () => {
+      try {
+        const respone = await apiCall.get('/folder')
+
+        setFolderList(respone)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    fetchFolder()
+  }, [])
+
+  const onClickFolder = (folderId: number) => {
+    changeDisplayFolder(setDisplayFolder, folderId)
+  }
+
+  return (
+    <SideMenuWrap>
+      <MenuList>
+        {folderList.map((folder: any) => (
+          <MenuItem onClick={() => onClickFolder(folder.id)} key={folder.id}>
+            {folder.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </SideMenuWrap>
+  )
+}
+
+export default SideMenu
+
+const SideMenuWrap = styled.div`
+  width: 200px;
+  height: 100%;
+  /* background-color: red; */
+  padding: 1rem;
+  border-right: 1px solid #333;
+`
+
+const MenuList = styled.ul``
+
+const MenuItem = styled.li``
