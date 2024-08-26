a0731c2320d83de10751882bc0870f676559911c
남균 <gyuni9604@naver.com>
2024-08-26 23:49:06 +0900

    추가 : 타입파일 추가

---
+++ memo-front/src/types/type.ts
@@ -0,0 +1,11 @@
+export interface MemoProps {
+  id: string | undefined
+  title: string
+  content: string
+  folderId: string
+}
+
+export interface MemoFolderProps {
+  id: string
+  name: string
+}
